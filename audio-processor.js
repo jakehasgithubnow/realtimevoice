e16526d03222abdff6c5d16e6c7af967767c6575
jakehasgithubnow <94901762+jakehasgithubnow@users.noreply.github.com>
2024-10-02 16:23:48 +0100

    Create audio-processor.js

---
+++ audio-processor.js
@@ -0,0 +1,10 @@
+class AudioProcessor extends AudioWorkletProcessor {
+    process(inputs, outputs, parameters) {
+        const input = inputs[0];
+        const inputChannel = input[0];
+        this.port.postMessage(inputChannel);
+        return true;
+    }
+}
+
+registerProcessor('audio-processor', AudioProcessor);
